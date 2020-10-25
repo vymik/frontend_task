3b7476b5b97073a8fe668a829a342223a462d0fa
Vytautas Mikalonis <vytautas.mikalonis@geniussports.com>
2020-10-26 00:31:24 +0200

    Updates

---
+++ precache-manifest.6fe9b1f1fa8fb1c5355b17214849f7b2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e7c71b6848c11117b52ce60a3bb0be",
+    "url": "/frontend_task/index.html"
+  },
+  {
+    "revision": "ca451b84fdc97e9eeb21",
+    "url": "/frontend_task/static/css/main.a525798a.chunk.css"
+  },
+  {
+    "revision": "0225c8b13c0a8e1678e9",
+    "url": "/frontend_task/static/js/2.3208012f.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/frontend_task/static/js/2.3208012f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca451b84fdc97e9eeb21",
+    "url": "/frontend_task/static/js/main.1140f0e3.chunk.js"
+  },
+  {
+    "revision": "6872b9b1c2eaa21fa3ab",
+    "url": "/frontend_task/static/js/runtime-main.396bffcd.js"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/frontend_task/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "2e26a9163cb4974dcba1bea5107d4492",
+    "url": "/frontend_task/static/media/Lato-BlackItalic.2e26a916.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/frontend_task/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "f98d18040a766b7bc4884b8fcc154550",
+    "url": "/frontend_task/static/media/Lato-BoldItalic.f98d1804.ttf"
+  },
+  {
+    "revision": "7582e823ef0d702969ea0cce9afb326d",
+    "url": "/frontend_task/static/media/Lato-Italic.7582e823.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/frontend_task/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "4d80ac573c53d192dafd99fdd6aa01e9",
+    "url": "/frontend_task/static/media/Lato-LightItalic.4d80ac57.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/frontend_task/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "4c44789d248c357144947b1ed3294430",
+    "url": "/frontend_task/static/media/mountain.4c44789d.jpg"
+  }
+]);
